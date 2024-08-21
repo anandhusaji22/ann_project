b7390ae6e3434b92ed61bf8a123355acc3a8b4f6
Akshay Shaji <akshayshaji02@gmail.com>
2024-08-21 15:51:02 +0530

    compleated vedio display

--- ann.py
+++ ann.py
@@ -41,30 +41,69 @@ def resize(img):
                                     borderType=cv2.BORDER_CONSTANT, value=[0, 0, 0])
     return padded_image
 
-#%%
-
-def getSymbols(image):
+def symbol_position(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Apply thresholding to create a binary image
-    _, binary = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY_INV)
+    _, binary = cv2.threshold(image, 100, 255, cv2.THRESH_BINARY_INV)
+    cv2.imshow('blacky', binary)
 
     # Find contours
     contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Minimum area threshold (adjust this value based on your needs)
-    min_area = 150
+    min_area = 50
 
     filtered_contours = [contour for contour in contours if cv2.contourArea(contour) > min_area]
     symbols_loc = [cv2.boundingRect(contour) for contour in filtered_contours]
     symbols_loc.sort(key= lambda x : x[0] )
-    symbols = [resize(binary[y:y+h, x:x+w]) for x, y, w, h in symbols_loc]
-    return symbols
+    return symbols_loc,binary
+
+
 
 #%%
+def vedioTracking():
+    while True:
+        _, frame = cap.read() 
+
+        # Flip image 
+        # frame = cv2.flip(frame, 1) 
+        # Draw a rectangle on the frame
+        x, y, w, h = 100, 100, 200, 100  # Example values (x, y) is top-left and (w, h) is width and height
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cropped_frame = frame[y:y+h, x:x+w]
+        # Process the image to get the position and binary mask
+        pos, binary = symbol_position(cropped_frame)
+
+        # Display text at the specified position
+        text = "=3"
+        if pos:
+            x_pos, y_pos,w_pos,h_pos = pos[-1]  # Assuming `pos` contains the (x, y) position
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 1
+            color = (255, 255, 255)  # White color
+            thickness = 2
+            # Draw the text on the image
+            cv2.putText(frame, text, (x_pos+x+w_pos+20, y_pos+y+h_pos), font, font_scale, color, thickness)
+
+
+
+        
+        # Optionally, display the frame with the text (e.g., in a window)
+        cv2.imshow('Frame with Text', frame)
+        if cv2.waitKey(1) & 0xff == ord('q'): 
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+#%%
+vedioTracking()
 image = cv2.imread(r'C:\Mathlab\my code\ANN\annpro\ann_project\img.jpg')
-s=getSymbols(image)
+pos,binary=symbol_position(image)
+symbols = [resize(binary[y:y+h, x:x+w]) for x, y, w, h in pos]
+
 count=1
-for sym in s:
-    showimg(sym)
-    # cv2.imwrite(f'symbol{count}.png', sym)
+f = "C:\\Mathlab\\my code\\ANN\\annpro\\ann_project\\images\\"
+for sym in symbols:
+    # showimg(sym)
+    # cv2.imwrite(f'{f}\symbol{count}.png', sym)
     count+=1
